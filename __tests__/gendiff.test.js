e95a4885d989b2657261b7e46cd3f636d9fd9c84
Anton Smolko <asmolko@yandex.ru>
2020-11-27 22:05:50 +0300

    reduced cognitive load in gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,24 +9,22 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const resultMap = {
-  stylish: readFile('result_stylish.txt'),
-  plain: readFile('result_plain.txt'),
-  json: readFile('result_json.txt'),
-};
+const stylishResult = readFile('result_stylish.txt');
+const plainResult = readFile('result_plain.txt');
+const jsonResult = readFile('result_json.txt');
 
-const formats = ['json', 'yml'];
-
-const matrix = Object.keys(resultMap).reduce((acc, formatter) => (
-  [...acc, ...formats.map((format) => [formatter, format])]
-), []);
+const formats = [
+  ['json'],
+  ['yml'],
+];
 
 test
-  .each(matrix)('Test genDiff with %s formatter, %s file format', (formatter, format) => {
+  .each(formats)('Test genDiff with .%s files', (format) => {
     const path2 = getFixturePath(`file2.${format}`);
     const path1 = getFixturePath(`file1.${format}`);
 
-    const diff = genDiff(path1, path2, formatter);
-    const expected = resultMap[formatter];
-    expect(diff).toEqual(expected);
+    expect(genDiff(path1, path2)).toEqual(stylishResult);
+    expect(genDiff(path1, path2, 'stylish')).toEqual(stylishResult);
+    expect(genDiff(path1, path2, 'plain')).toEqual(plainResult);
+    expect(genDiff(path1, path2, 'json')).toEqual(jsonResult);
   });
